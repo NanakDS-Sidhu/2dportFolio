b7daab0fe70639a40b6848a8ce30609a3250b773
NanakDS-Sidhu <sidhu.nds22@gmail.com>
2025-04-29 23:34:18 +0530

    uno

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
-k.loadSprite("spriresheet","./spritesheet.png",{
-    sliceX : 39,
-    sliceY : 31,
+k.loadSprite("spritesheet", "./spritesheet.png", {
+    sliceX: 39,
+    sliceY: 31,
     anims: {
-        "idle-down": 936 ,
-        "walk-down" :{from : 936, to:939,loop:true,speed:8},
-        "idle-side": 975 ,
-        "walk-side" :{from : 975, to:978,loop:true,speed:8},
-        "idle-up": 1014 ,
-        "walk-side" :{from : 1014, to:1017,loop:true,speed:8},
+      "idle-down": 936,
+      "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+      "idle-side": 975,
+      "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+      "idle-up": 1014,
+      "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
     },
-
-})
-
-k.load("map","./map.png")
-
-k.setBackground(k.Color.fromHex("#311047"));
+  });
+  
+  k.loadSprite("map", "./map.png");
+  
+  k.setBackground(k.Color.fromHex("#311047"));
 
 k.scene("main",async ()=>{
     const mapData = await (await fetch("./map.json")).json()
@@ -47,7 +46,7 @@ k.scene("main",async ()=>{
     ])
 
     for (const layer of layers){
-        if (layer.name=="boundaries"){
+        if (layer.name ==="boundaries"){
             for (const boundary of layer.objects){
                 map.add([
                     k.area({
