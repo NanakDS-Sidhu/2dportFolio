317f014ac0929a95cb12625a78d32c1e302623c4
NanakDS-Sidhu <sidhu.nds22@gmail.com>
2025-04-30 12:03:48 +0530

    minor changes

--- src/constants.js
+++ src/constants.js
@@ -4,21 +4,21 @@ export const dialogueData = {
   PC: `This is my PC. I work mostly in JavaScript and Python these days.
     I've made many projects in these languages. Anyway regardless of the language, I just like programming.
     Here is my <a href="https://github.com/NanakDS-Sidhu" target="_blank">Github</a>!`,
-  Degree: `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-  Sofa: `That's my sofa. I like to relax here and watch YouTube.`,
-  TV: `That's my TV. I've been watching a lot of youtube here`,
-  Bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  Degree: `This is my CS degree. I graduated from Punjab Engineering College, learnt a lot from my professors.`,
+  Sofa: `That's my sofa. I like to relax here and read up on mathematical literature`,
+  TV: `That's my TV. I enjoy watching Sci-fi movies`,
+  Bed: `This where I sleep. 8hrs a day andI have the strength to work hard all day long`,
   Resume: `This is my desk and on it is my resume. <a href="https://github.com/NanakDS-Sidhu/resume/blob/a7a4a0dbb517c48c24f37fe714158c1c261b2529/Nanak%20REsume.pdf" target="_blank">Check it out?</a>
   Contact me at sidhu.nds22@gamil.com if you have any interesting job opportunities!`,
   Projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
-  Library: `There are a lot of books on my shelves. My favourites are Dune , Notes from the underground , Lord of the rings and Crime and Punishment.
+  Library: `There are a lot of books on my shelves. My favourites are "Dune" and "Crime and Punishment".
   Which ones do you like?`,
   Chest:`I store my precious stuff here but I lost the key`,
   Plant:`Ahh I love my plants. Can't think of a nickname for them though`,
   Fridge:`The perfect time for a snack is always`,
   BeanBag:`The chill spot with my trusty bean bag`,
-  Key:`Wonder what this opens`,
+  Key : `Wonder what this opens`,
   Ghost:`This house may be haunted but he's my friend`,
   Grave:`A replica of Rosetta stone, still crazy how useful a coincidence can be`,
   Hole:`Wonder what it leads to. <a href="https://minecraft-js-six.vercel.app/" target="_blank">Jump in?</a>`,
